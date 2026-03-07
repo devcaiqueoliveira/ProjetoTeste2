d9e434615a47181f49bf1c40312f178ff0aff974
Caique Oliveira <devcaiqueoliveira@gmail.com>
2026-03-07 08:22:52 -0300

    resolucao projeto

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ola, mundo!");
-        System.out.println("Ambiente de Desenvolvimento Funcionando");
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> listaDeNomes = new ArrayList<>();
+
+        boolean continuar = true;
+
+        System.out.println("--- LISTA DE PRESENÇA ---");
+
+        while (continuar) {
+            System.out.println("1. Adicionar aluno");
+            System.out.println("2. Ver lista de presença");
+            System.out.println("3. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            String opcao = scanner.nextLine();
+
+            switch (opcao) {
+                case "1" -> {
+                    System.out.print("Digite o nome do aluno: ");
+                    String nomeDoAluno = scanner.nextLine();
+                    listaDeNomes.add(nomeDoAluno);
+                }
+                case "2" -> {
+                    System.out.println("--- Exibindo lista ---");
+                    if (listaDeNomes.isEmpty()) {
+                        throw new RuntimeException("Lista vazia");
+                    } else {
+                        for (String listaDeNome : listaDeNomes) {
+                            System.out.println(listaDeNome);
+                        }
+                    }
+                }
+                case "3" -> {
+                    System.out.println("Saindo...");
+                    continuar = false;
+                }
+                default -> {
+                    System.out.println("Digite uma opção válida.");
+                }
+            }
+        }
+        scanner.close();
     }
 }
